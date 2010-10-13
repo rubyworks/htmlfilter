5bf56d852bb8f422a875bc0ffb8a3752d88019dd
7rans <transfire@gmail.com>
2010-10-13 16:43:36 -0400

    Updated files.

--- assets/main.js
+++ assets/main.js
@@ -105,12 +105,3 @@ function highlightClickTarget( event ) {
 	};
 };
 
-
-$(document).ready( function() {
-	hookSourceViews();
-	hookDebuggingToggle();
-	hookQuickSearch();
-	highlightLocationTarget();
-
-	$('ul.link-list a').bind( "click", highlightClickTarget );
-});
